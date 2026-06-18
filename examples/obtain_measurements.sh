f6b9efd5f1a797532521664085b6c7f002d9b4b3
dragos-ionescu <tools@ionescudragos.com>
2026-06-18 12:03:01 +0200

    nice to niceness in examples

--- examples/obtain_measurements.sh
+++ examples/obtain_measurements.sh
@@ -2,6 +2,6 @@
 green-languages \
     {internal,process}/{c,cs,java,rust}/{division-loop,matrix-multiplication,polynomial-evaluation}.yml \
     --rapl --misses --cycles --cstates \
-    --internal-runs 5 --runs 2 --cooldown 5000
+    --internal-runs 5 --cooldown 5000
     --output "obtained-measurements"
 
